1d7ce72ddb0ee49508f46c3ccc9bb8b974a4c120
SeoHyeokGyu <shk64099@gmail.com>
2021-04-08 20:13:16 +0900

    모델(테이블) 생성, Hibernate (1) User,Board,Reply (2) 연관관계 설정

---
+++ src/main/java/com/myblog/blog/model/Board.java
@@ -0,0 +1,45 @@
+package com.myblog.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private  String title;
+
+    @Lob //대용량 데이터
+    private String content; // 라이브러리 사용예정
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+    private List<Reply> reply;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
---
+++ src/main/java/com/myblog/blog/model/User.java
@@ -0,0 +1,40 @@
+package com.myblog.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB의 넘버링 전략을 따라간다.
+    private int id; // 시퀀스
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private  String email;
+
+    @ColumnDefault("'user'")
+    private String role; //Enum 사용해함, admin,user,manager
+
+    @CreationTimestamp // 시간이 자동으로 입력
+    private Timestamp createDate;
+
+}
